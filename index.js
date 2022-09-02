67f5c346f7ae546508f962939a894dc68027a87b
Commandtechno <68407783+Commandtechno@users.noreply.github.com>
2022-09-02 01:09:33 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+export default {
+  name: "esbuild-plugin-macros",
+  setup(build) {
+    build.onLoad({ filter: /\.macro\.[^\.]+$/ }, async args => {
+      const code = await esbuild
+        .build({
+          entryPoints: [args.path],
+          bundle: true,
+          write: false,
+          watch: true,
+          outfile: "",
+          format: "esm"
+        })
+        .then(result => result.outputFiles[0].text);
+
+      const exports = await import("data:text/javascript;charset=utf-8;base64," + btoa(code));
+      return { loader: "ts", contents: "export = " + serialize(exports) };
+    });
+  }
+};
